aa0e21a4bb0089f45d4a4e10e5eb9f253f5ba49b
Miftah <miftahaadem@gmail.com>
2024-07-21 08:24:26 +0300

    second commit

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
+  const addButton = document.getElementById('add-task-btn')
   const taskInput = document.getElementById('task-input')
-  const addTaskButton = document.getElementById('add-task-btn')
   const taskList = document.getElementById('task-list')
 
-  addTaskButton.addEventListener('click', addTask)
+  function loadTasks() {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]')
+    storedTasks.forEach((taskText) => addTask(taskText, false))
+  }
 
-  function addTask() {
+  function addTask(taskText, save = true) {
     const taskText = taskInput.value.trim()
-
     if (taskText === '') {
-      alert('Please enter a task')
-      return
+      alert('Please enter a task.')
     }
-
     const listItem = document.createElement('li')
     listItem.textContent = taskText
 
     const removeButton = document.createElement('button')
-    removeButton.classList.add('remove-btn')
     removeButton.textContent = 'Remove'
-    removeButton.addEventListener('click', () => {
-      taskList.removeChild(listItem)
+    removeButton.classList.add('remove-btn')
+    removeButton.addEventListener('click', function () {
+      listItem.remove()
+      removeTaskFromStorage(taskText)
     })
-
     listItem.appendChild(removeButton)
     taskList.appendChild(listItem)
     taskInput.value = ''
+    if (save) {
+      const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]')
+      storedTasks.push(taskText)
+      localStorage.setItem('tasks', JSON.stringify(storedTasks))
+    }
   }
+
+  function removeTaskFromStorage(taskText) {
+    let storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]')
+    storedTasks = storedTasks.filter((task) => task !== taskText)
+    localStorage.setItem('tasks', JSON.stringify(storedTasks))
+  }
+
+  addButton.addEventListener('click', function () {
+    const taskText = taskInput.value.trim()
+    if (taskText !== '') {
+      addTask(taskText)
+    } else {
+      alert('Please enter a task.')
+    }
+  })
+  taskInput.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+      const taskText = taskInput.value.trim()
+      if (taskText !== '') {
+        addTask(taskText)
+      } else {
+        alert('Please enter a task.')
+      }
+    }
+    loadTasks()
+  })
 })
